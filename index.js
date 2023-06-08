85a71df1838e41d1fb491c78bd24e94860cfd093
Syahrul Safarila <sysafarila.official@gmail.com>
2023-06-08 22:00:43 +0700

    install express

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-const { Client } = require('whatsapp-web.js')
+const { Client, LocalAuth } = require('whatsapp-web.js')
 const qrcode = require('qrcode-terminal')
+const express = require('express')
 
-const client = new Client()
+const app = express()
+const port = 3000
+const client = new Client({
+    authStrategy: new LocalAuth()
+})
+
+app.use(express.json())
+
+app.post('/send', (req, res) => {
+    const phoneNumber = req.body.phoneNumber
+    if (!phoneNumber) {
+        return res.send('phoneNumber required')
+    }
+    const message = req.body.message ?? 'Default Message!'
+
+    const whatsapp = client.sendMessage(`${phoneNumber}@c.us`, message)
+
+    res.send(whatsapp)
+})
 
 client.on('qr', (qr) => {
     console.log('QR RECEIVED', qr)
@@ -15,12 +34,15 @@ client.on('ready', () => {
 })
 
 client.on('message', (msg) => {
-    console.log(msg)
-    msg.reply('Sending test message to +6285155001528')
-    client.sendMessage('6285155001528@c.us', 'test')
+    if (msg.body == '!ping') {
+        msg.reply('pong')
+    }
 })
 
 client.initialize()
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
 
 // send message to specific user number, start the number with country code and end it with "@c.us"
 // client.sendMessage('6285155001528@c.us', 'your message here')
